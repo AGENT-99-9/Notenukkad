833056ae3705e782562ce0ace2ce531f1f40ea77
ANKUR MAURYA <ak.maurya21jackk@gmail.com>
2025-12-03 12:15:29 +0530

    allowed railway host

--- notenukkad/settings.py
+++ notenukkad/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key-change-me')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 
-ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['notenukkad-production.up.railway.app', '127.0.0.1', 'localhost']
 
 
 
